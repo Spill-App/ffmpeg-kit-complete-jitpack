96b1e6a57c2c10b3f33ae8fc7268221f084ad167
damonique.dev <damoniquethomas1@gmail.com>
2025-05-21 15:49:14 -0400

    Remove JSONObject

--- src/main/java/com/arthenica/ffmpegkit/MediaInformation.java
+++ src/main/java/com/arthenica/ffmpegkit/MediaInformation.java
@@ -1,16 +1,23 @@
 package com.arthenica.ffmpegkit;
 
-import com.google.gson.JsonObject;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * Media information class.
  */
 public class MediaInformation {
-    private JSONObject allProperties;
+    // Use Map<String, Object> instead of JSONObject
+    private Map<String, Object> allProperties;
     
-    public JSONObject getAllProperties() {
+    /**
+     * Get all properties.
+     * 
+     * @return Map containing all properties
+     */
+    public Map<String, Object> getAllProperties() {
         if (allProperties == null) {
-            allProperties = new JSONObject();
+            allProperties = new HashMap<>();
         }
         return allProperties;
     }
